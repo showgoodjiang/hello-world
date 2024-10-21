72dc1086ad574975969df21ee3d514d063403721
Jeremy Jiang <showgoodjiang@163.com>
2024-10-21 15:05:31 +0800

    About page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Hi, welcome to Your Vue App"/>
   </div>
 </template>
 
